87dc6ed4c733e3a1f384c259a573e3d27060bebe
Aleksas Hauser <aleksas.hauser@gmail.com>
2025-04-04 23:54:29 -0400

    Correctly raise NotImplementedErrors

--- model/metamodel.py
+++ model/metamodel.py
@@ -193,7 +193,8 @@ class LimitClause(Clause):
         return visitor.visit_limit_clause(self, parameter)
 
 class Executable(ABC):
-    pass
+    def visit[P, T](self, visitor: ExecutionVisitor, parameter: P) -> T:
+        pass
 
 @dataclass
 class Query(Executable):
@@ -271,108 +272,108 @@ class DataFrame(ABC):
 class ExecutionVisitor(ABC):
     @abstractmethod
     def visit_runtime[P, T, R: Runtime](self, val: R, parameter: P) -> T:
-        raise NotImplementedError
+        raise NotImplementedError()
 
     @abstractmethod
     def visit_query[P, T](self, val: Query, parameter: P) -> T:
-        raise NotImplementedError
+        raise NotImplementedError()
 
     @abstractmethod
     def visit_integer_literal[P, T](self, val: IntegerLiteral, parameter: P) -> T:
-        raise NotImplementedError
+        raise NotImplementedError()
 
     @abstractmethod
     def visit_string_literal[P, T](self, val: StringLiteral, parameter: P) -> T:
-        raise NotImplementedError
+        raise NotImplementedError()
 
     @abstractmethod
     def visit_boolean_literal[P, T](self, val: BooleanLiteral, parameter: P) -> T:
-        raise NotImplementedError
+        raise NotImplementedError()
 
     @abstractmethod
     def visit_operand_expression[P, T](self, val: OperandExpression, parameter: P) -> T:
-        raise NotImplementedError
+        raise NotImplementedError()
 
     @abstractmethod
     def visit_not_unary_operator[P, T](self, val: NotUnaryOperator, parameter: P) -> T:
-        raise NotImplementedError
+        raise NotImplementedError()
 
     @abstractmethod
     def visit_equals_binary_operator[P, T](self, val: EqualsBinaryOperator, parameter: P) -> T:
-        raise NotImplementedError
+        raise NotImplementedError()
 
     @abstractmethod
     def visit_not_equals_binary_operator[P, T](self, val: NotEqualsBinaryOperator, parameter: P) -> T:
-        raise NotImplementedError
+        raise NotImplementedError()
 
     @abstractmethod
     def visit_greater_than_binary_operator[P, T](self, val: GreaterThanBinaryOperator, parameter: P) -> T:
-        raise NotImplementedError
+        raise NotImplementedError()
 
     @abstractmethod
     def visit_greater_than_equals_operator[P, T](self, val: GreaterThanEqualsBinaryOperator, parameter: P) -> T:
-        raise NotImplementedError
+        raise NotImplementedError()
 
     @abstractmethod
     def visit_less_than_binary_operator[P, T](self, val: LessThanBinaryOperator, parameter: P) -> T:
-        raise NotImplementedError
+        raise NotImplementedError()
 
     @abstractmethod
     def visit_less_than_equals_binary_operator[P, T](self, val: LessThanEqualsBinaryOperator, parameter: P) -> T:
-        raise NotImplementedError
+        raise NotImplementedError()
 
     @abstractmethod
     def visit_and_binary_operator[P, T](self, val: AndBinaryOperator, parameter: P) -> T:
-        raise NotImplementedError
+        raise NotImplementedError()
 
     @abstractmethod
     def visit_or_binary_operator[P, T](self, val: OrBinaryOperator, parameter: P) -> T:
-        raise NotImplementedError
+        raise NotImplementedError()
 
     @abstractmethod
     def visit_literal_expression[P, T](self, literalExpression: LiteralExpression, parameter: P) -> T:
-        raise NotImplementedError
+        raise NotImplementedError()
 
     @abstractmethod
     def visit_unary_expression[P, T](self, val: UnaryExpression, parameter: P) -> T:
-        raise NotImplementedError
+        raise NotImplementedError()
 
     @abstractmethod
     def visit_binary_expression[P, T](self, val: BinaryExpression, parameter: P) -> T:
-        raise NotImplementedError
+        raise NotImplementedError()
 
     @abstractmethod
     def visit_reference_expression[P, T](self, val: ReferenceExpression, parameter: P) -> T:
-        raise NotImplementedError
+        raise NotImplementedError()
 
     @abstractmethod
     def visit_function_expression[P, T](self, val: FunctionExpression, parameter: P) -> T:
-        raise NotImplementedError
+        raise NotImplementedError()
 
     @abstractmethod
     def visit_filter_clause[P, T](self, val: FilterClause, parameter: P) -> T:
-        raise NotImplementedError
+        raise NotImplementedError()
 
     @abstractmethod
     def visit_selection_clause[P, T](self, val: SelectionClause, parameter: P) -> T:
-        raise NotImplementedError
+        raise NotImplementedError()
 
     @abstractmethod
     def visit_extend_clause[P, T](self, val: ExtendClause, parameter: P) -> T:
-        raise NotImplementedError
+        raise NotImplementedError()
 
     @abstractmethod
     def visit_group_by_clause[P, T](self, val: GroupByClause, parameter: P) -> T:
-        raise NotImplementedError
+        raise NotImplementedError()
 
     @abstractmethod
     def visit_distinct_clause[P, T](self, val: DistinctClause, parameter: P) -> T:
-        raise NotImplementedError
+        raise NotImplementedError()
 
     @abstractmethod
     def visit_limit_clause[P, T](self, val: LimitClause, parameter: P) -> T:
-        raise NotImplementedError
+        raise NotImplementedError()
 
     @abstractmethod
     def visit_join_clause[P, T](self, val: JoinClause, parameter: P) -> T:
-        raise NotImplementedError
+        raise NotImplementedError()
